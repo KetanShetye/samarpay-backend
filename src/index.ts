5b7f1323345aa3eea4dd7fdcf9e13792f4bd6a59
KetanShetyeMayking <ketan@mayking.in>
2025-09-08 11:24:07 +0530

    added one more test route

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,10 @@ app.get("/", (req:any, res:any) => {
   res.send("Samarpay Backend is running on port 8000!");
 });
 
+app.get("/inital-commit", (req:any, res:any) => {
+  res.send("initial-commit route added");
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
